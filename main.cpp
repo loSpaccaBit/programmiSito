17e7a4a174f89847271ecdecafcd288379bd6d30
Francesco Pio Nocerino <91205851+KekkoDev@users.noreply.github.com>
2022-12-27 18:01:46 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -13,17 +13,7 @@ using namespace std;
 
 int main()
 {
-    int l;
-    cout << "Inserisci Lato > ";
-    cin >> l;
-    for (int i = 1; i <= l; i++)
-    {
-        for (int j = 1; j <= i; j++)
-        {
-            cout << "*";
-        }
-        cout << endl;
-    }
+    cout<<"Hello World";
 
     return 0;
 }
